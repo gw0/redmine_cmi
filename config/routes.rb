23bfeef2479fe7799a422e2b50ed5641185eea34
Pablo Moreira Mora <pablo.moreiramora@gmail.com>
2013-02-25 12:01:03 -0300

    Fix routes for rails 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,23 @@
-ActionController::Routing::Routes.draw do |map|
-  map.resources :checkpoints,
-                :path_prefix => '/projects/:project_id/metrics',
-                :member => { :new_journal => :post, :edit_journal => [:get, :post] },
-                :collection => { :preview => :put }
-  map.resources :expenditures,
-                :path_prefix => '/projects/:project_id/metrics',
-                :member => { :new_journal => :post, :edit_journal => [:get, :post] },
-                :collection => { :preview => :put }
-  map.metrics '/projects/:project_id/metrics/:action', :controller => 'metrics'
-  map.management '/management/:action', :controller => 'management'
-  map.cost_history '/admin/cost_history', :controller => 'admin', :action => 'cost_history'
+RedmineApp::Application.routes.draw do
+  resources :checkpoints, :path_prefix => '/projects/:project_id/metrics' do
+    :member do
+      post :new_journal
+      post :edit_journal
+      get :edit_journal
+    end
+    put :preview, :on => :collection
+  end
+
+  resources :expenditures, :path_prefix => '/projects/:project_id/metrics' do
+    :member do
+      post :new_journal
+      post :edit_journal
+      get :edit_journal
+    end
+    put :preview, :on => :collection
+  end
+
+  match '/projects/:project_id/metrics/:action', :controller => 'metrics', :as => :metrics
+  match '/management/:action', :controller => 'management', :as => :management
+  match '/admin/cost_history', :controller => 'admin', :action => 'cost_history', :as => :cost_history
 end
